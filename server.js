968c0b545485026a430c8aa2b5954f5a8124dbca
faridasad <asadlifaridd@gmail.com>
2023-04-13 14:49:06 +0400

    Add routes

--- server.js
+++ server.js
@@ -1,58 +1,119 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { config } = require("dotenv");
+config();
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+const requireLogin = require('./middlewares/requireLogin.js');
 
-const rooms = { }
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "mysecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get('/', (req, res) => {
-  res.render('index', { rooms: rooms })
-})
+const rooms = {};
+const users = {};
 
-app.post('/room', (req, res) => {
+app.get("/", (req, res) => {
+  if (req.session.user) {
+    return res.redirect("/index");
+  }
+  res.render("login", { admin: process.env.ADMIN, pass: process.env.PASS });
+});
+
+app.post("/index", (req, res, next) => {
+  if (!req.body.password) {
+    req.session.user = {
+      username: req.body.username,
+      isAdmin: false,
+    }
+
+    return res.redirect("/index");
+  }
+
+  if (
+    req.body.username === process.env.ADMIN &&
+    req.body.password === process.env.PASS
+  ) {
+    req.session.user = {
+      username: req.body.username,
+      isAdmin: true,
+    }
+    return res.redirect("/index");
+  }
+
+  return res.redirect("/");
+});
+
+app.get("/index", requireLogin, (req, res) => {
+  res.render("index", { rooms: rooms, user: req.session.user });
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(500).send("An error occurred");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.post("/room", (req, res) => {
   if (rooms[req.body.room] != null) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  rooms[req.body.room] = { users: {} }
-  res.redirect(req.body.room)
-  // Send message that new room was created
-  io.emit('room-created', req.body.room)
-})
+  rooms[req.body.room] = { users: {} };
+  res.redirect(req.body.room);
+  io.emit("room-created", req.body.room);
+});
 
-app.get('/:room', (req, res) => {
+app.get("/:room", (req, res) => {
   if (rooms[req.params.room] == null) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  res.render('room', { roomName: req.params.room })
-})
+  res.render("room", { roomName: req.params.room, username: req.session.user.username });
+});
 
-server.listen(3000)
+server.listen(3000);
 
-io.on('connection', socket => {
-  socket.on('new-user', (room, name) => {
-    socket.join(room)
-    rooms[room].users[socket.id] = name
-    socket.to(room).broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
-      delete rooms[room].users[socket.id]
-    })
-  })
-})
+// Socket
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (room, name) => {
+    console.log(name)
+    socket.join(room);
+    rooms[room].users[socket.id] = name;
+    socket.to(room).broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (room, message) => {
+    socket.to(room).broadcast.emit("chat-message", {
+      message: message,
+      name: rooms[room].users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    getUserRooms(socket).forEach((room) => {
+      socket
+        .to(room)
+        .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
+      delete rooms[room].users[socket.id];
+    });
+  });
+});
 
 function getUserRooms(socket) {
   return Object.entries(rooms).reduce((names, [name, room]) => {
-    if (room.users[socket.id] != null) names.push(name)
-    return names
-  }, [])
+    if (room.users[socket.id] != null) names.push(name);
+    return names;
+  }, []);
 }
