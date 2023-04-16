6537cf27b251763cdb0be003edaf949a32adee26
faridasad <asadlifaridd@gmail.com>
2023-04-16 15:47:13 +0400

    Improve logic

--- public/script.js
+++ public/script.js
@@ -1,46 +1,82 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const roomContainer = document.getElementById('room-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
+const roomContainer = document.getElementById("room-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button")
 
 if (messageForm != null) {
-  appendMessage('You joined')
-  socket.emit('new-user', roomName, username)
+  appendMessage({ message: "You joined" });
+  socket.emit("new-user", roomName, username);
 
-  messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message', roomName, message)
-    messageInput.value = ''
-  })
+  messageForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    sendButton.disabled = true;
+    const message = messageInput.value;
+    const lastMessage = messageContainer.lastChild;
+    lastMessage.classList.contains("author")
+      ? appendMessage({ message, isAuthor: true })
+      : appendMessage({ message, isAuthor: true, who: "You" });
+    socket.emit("send-chat-message", roomName, message);
+    messageInput.value = "";
+    setTimeout(() => {
+      sendButton.disabled = false;
+    }, 1000);
+  });
 }
 
-socket.on('room-created', room => {
-  const roomElement = document.createElement('div')
-  roomElement.innerText = room
-  const roomLink = document.createElement('a')
-  roomLink.href = `/${room}`
-  roomLink.innerText = 'Join'
-  roomContainer.append(roomElement)
-  roomContainer.append(roomLink)
-})
+socket.on("room-created", (room) => {
+  const roomElement = document.createElement("div");
+  roomElement.innerText = room;
+  const roomLink = document.createElement("a");
+  roomLink.href = `/${room}`;
+  roomLink.innerText = "Join";
+  roomContainer.append(roomElement);
+  roomContainer.append(roomLink);
+});
 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
+socket.on("chat-message", (data) => {
+  const lastMessage = messageContainer.lastChild;
+  const currentAuthor = lastMessage.getAttribute("data-author");
+  appendMessage({
+    message: data.message,
+    isAuthor: false,
+    who: data.name,
+    lastAuthor: currentAuthor,
+  });
+});
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
+socket.on("user-connected", (name) => {
+  appendMessage({ message: `${name} connected` });
+});
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
+socket.on("user-disconnected", (name) => {
+  appendMessage({ message: `${name} disconnected` });
+});
 
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+function appendMessage({ message, isAuthor, who, lastAuthor }) {
+  console.log(lastAuthor, who);
+  const messageElement = document.createElement("div");
+  const messageText = document.createElement("span");
+  messageText.classList.add("message");
+
+  if (who && who !== lastAuthor) {
+    const authorName = document.createElement("span");
+    authorName.classList.add("author-name");
+    authorName.textContent = who;
+    messageElement.append(authorName);
+  }
+
+  isAuthor && messageElement.classList.add("author");
+
+  messageElement.setAttribute("data-author", who);
+
+  messageText.textContent = message;
+  messageElement.append(messageText);
+  messageContainer.append(messageElement);
 }
+
+window.history.pushState(null, null, document.URL);
+window.addEventListener("popstate", function () {
+  window.history.pushState(null, null, document.URL);
+});
